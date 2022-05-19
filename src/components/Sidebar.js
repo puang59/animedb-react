be1f84f6fb03a75ad15df63539c70d32d41500e1
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-05-19 14:14:01 +0530

    initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+function Sidebar({ topAnime }) {
+    return (
+        <aside>
+            <nav>
+                <h3>Top Anime</h3>
+                {topAnime.map(anime => (
+                    <a href={anime.url} target="_blank" rel="norefferer" key={anime.mal_id}>
+                        {anime.title}
+                    </a>
+                ))}
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar
